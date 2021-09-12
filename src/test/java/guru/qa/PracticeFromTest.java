5535b3edf32ae1cca8244091fb58b92777d41d6c
Bolgar <bolgar888@gmail.com>
2021-09-12 18:10:54 +0300

    PageObjects + JavaFaker

---
+++ src/test/java/guru/qa/PracticeFromTest.java
@@ -0,0 +1,55 @@
+package guru.qa;
+
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import guru.qa.pages.RegistrationPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$x;
+import static guru.qa.TestData.*;
+
+public class PracticeFromTest extends TestBase {
+    Faker faker = new Faker();
+    RegistrationPage registrationPage = new RegistrationPage();
+    String firstname = faker.name().firstName();
+    String lastname = faker.name().lastName();
+    String email = faker.internet().emailAddress();
+    String userNumber = faker.phoneNumber().subscriberNumber(10);
+    String address = faker.address().fullAddress();
+    String gender = "Other";
+    String subject = "Math";
+    String hobbie = "Sports";
+    String stateName = "NCR";
+    String cityName = "Delhi";
+
+    @Test
+    void testFormPractice() {
+        registrationPage.openPage();
+        registrationPage.typeFirstname(firstname);
+        registrationPage.typeLastName(lastname);
+        registrationPage.typeEmail(email);
+        registrationPage.typeGenderMale(gender);
+        registrationPage.typeNumber(userNumber);
+        registrationPage.calendar.setDate("28", "July", "2005");
+        registrationPage.typeSubject(subject);
+        registrationPage.typeHobbie(hobbie);
+        registrationPage.typeAddress(address);
+        registrationPage.choiceState(stateName);
+        registrationPage.choiseCity(cityName);
+        registrationPage.submitForm();
+        // Проверка Формы
+        registrationPage.checkResult("Student Name", firstname + " " + lastname);
+        registrationPage.checkResult("Student Email", email);
+        registrationPage.checkResult("Gender", gender);
+        registrationPage.checkResult("Date of Birth", "28" + " " + "July" + "," + "2005");
+        registrationPage.checkResult("Subjects", subject);
+        registrationPage.checkResult("Hobbies", hobbie);
+        registrationPage.checkResult("Address", address);
+        registrationPage.checkResult("State and City", stateName + " " + cityName);
+
+    }
+}
---
+++ src/test/java/guru/qa/pages/RegistrationPage.java
@@ -0,0 +1,95 @@
+package guru.qa.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import guru.qa.pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationPage {
+
+    private SelenideElement formTitle = $(".practice-form-wrapper"),
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            emailInput = $("#userEmail"),
+            phoneNumber = $("#userNumber"),
+            subjectInput = $("#subjectsInput"),
+            pickHobbie = $("#hobbiesWrapper"),
+            addressInput = $("#currentAddress"),
+            selectState = $("#state"),
+            selectStateCity = $("#stateCity-wrapper"),
+            selectCity = $("#city"),
+            selectGender = $("#genterWrapper");
+
+    public CalendarComponent calendar = new CalendarComponent();
+
+    private final String FORM_TITLE = "Student Registration Form";
+    private final String SUBMIT_TITLE = "Thanks for submitting the form";
+
+    public void openPage() {
+        open("https://demoqa.com/automation-practice-form");
+        formTitle.shouldHave(text("Student Registration Form"));
+    }
+
+    public void typeFirstname(String value) {
+        firstNameInput.setValue(value);
+
+    }
+
+    public void typeLastName(String value) {
+        lastNameInput.setValue(value);
+
+    }
+
+    public void typeEmail(String value) {
+        emailInput.setValue(value);
+
+    }
+
+    public void typeGenderMale(String gender) {
+
+        selectGender.$(byText(gender)).click();
+    }
+
+    public void typeNumber(String value) {
+        phoneNumber.setValue(value);
+
+    }
+
+    public void typeSubject(String value) {
+        $("#subjectsInput").setValue(value).pressEnter();
+    }
+
+    public void typeHobbie(String hobbie) {
+        pickHobbie.$(byText(hobbie)).click();
+    }
+
+    public void typeAddress(String value) {
+        addressInput.setValue(value);
+    }
+
+    public void choiceState(String stateName) {
+        selectState.click();
+        selectStateCity.$(byText(stateName)).click();
+    }
+
+    public void choiseCity(String cityName) {
+        selectCity.click();
+        selectStateCity.$(byText(cityName)).click();
+
+    }
+
+    public void submitForm() {
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+    }
+
+    public void checkResult(String nameForm, String valueForm) {
+        $x("//td[text()=" + "'" + nameForm + "'" + "]/following-sibling::td").shouldHave(text(valueForm));
+    }
+}
+
+
+
+
